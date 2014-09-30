c0c553694c198a3966e457dd5f4755e913fbb6bc
Luis Doubrava <luis@cg.nl>
2014-09-30 10:19:37 +0200

    remove where from query for speed improvement

--- app/controllers/spree/feeds_controller.rb
+++ app/controllers/spree/feeds_controller.rb
@@ -5,7 +5,7 @@ module Spree
     def show
       params.merge!(per_page: 10000)
       @searcher = build_searcher(params)
-      @products = @searcher.retrieve_products.includes(:variants).includes(product_properties: :property).where('spree_properties.name' => ['brand', 'color']).includes(:prices).where('spree_prices.currency' => 'EUR')
+      @products = @searcher.retrieve_products.includes(:variants).includes(product_properties: :property).includes(:prices)
       options = %w(google beslist).include?(params[:id]) ? {template: "spree/feeds/#{params[:id]}", status: 200, layout: false} : {nothing: true, status: 404}
 
       respond_to do |format|
