47cc0722d66b09745acff8b128b0f2c5ebd35682
Luis Doubrava <luis@cg.nl>
2014-09-30 14:48:31 +0200

    add cache to feeds controller

--- app/controllers/spree/feeds_controller.rb
+++ app/controllers/spree/feeds_controller.rb
@@ -3,9 +3,19 @@ module Spree
     respond_to :xml
 
     def show
+      # p = Spree::Product.arel_table
+      # v = Spree::Variant.arel_table
+      # s = Spree::Store.arel_table
+      # ps = Arel::Table.new(:spree_products_stores)
+      # query = p.join(ps).on(ps[:product_id].eq(p[:id])).join(s).on(ps[:product_id].eq(p[:id])).where(ps[:store_id].eq(params[:current_store_id])).join(v).on(v[:product_id].eq(p[:id])).project(Arel.sql('*'))
+      # @products = Spree::Product.find_by_sql(query.to_sql)
       params.merge!(per_page: 10000)
-      @searcher = build_searcher(params)
-      @products = @searcher.retrieve_products.includes(:variants).includes(product_properties: :property).includes(:prices)
+      searcher = build_searcher(params)
+
+      @products = Rails.cache.fetch("product_feed_#{params[:id]}", expires_in: 1.hour, race_condition_ttl: 2.minutes) do
+        searcher.retrieve_products.includes(:variants).includes(product_properties: :property).includes(:prices).all
+      end
+
       options = %w(google beslist).include?(params[:id]) ? {template: "spree/feeds/#{params[:id]}", status: 200, layout: false} : {nothing: true, status: 404}
 
       respond_to do |format|
